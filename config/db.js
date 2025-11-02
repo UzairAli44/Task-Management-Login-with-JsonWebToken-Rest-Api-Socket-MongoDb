9d1b8a5113d2b6e4933715736b2560d9768dc53c
Uzair Ali <uzairaliuzi877@gmail.com>
2025-11-02 22:02:49 +0500

    working on to connect to db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const env = require('../environment/env');
 
-mongoose.connect(env.MONGODB_URI).then(() => {
+mongoose.connect('mongodb://localhost:27017/task_management_db').then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
     console.error('Error connecting to MongoDB:', err);
